66a990802e8c74cf04cb013ecbd879886ca4555d
Naufal Mahdi <naupal11746@gmail.com>
2021-04-02 22:57:37 +0700

    Upload

--- TUGAS3/belajarlaravel/routes/web.php
+++ TUGAS3/belajarlaravel/routes/web.php
@@ -13,19 +13,20 @@ use App\Http\Controllers\ManagementUserController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [ManagementUserController::class, 'index']);
+//Route::get('/', [ManagementUserController::class, 'index']);
 
 //Route::resource('user', [ManagementUserController::class, 'user']);  
 
-Route::get('/',[ManagementUserController::class, 'index']);
-Route::get('/create',[ManagementUserController::class, 'create']);
-Route::get('/store',[ManagementUserController::class, 'store']);
-Route::get('/show',[ManagementUserController::class, 'show']);
-Route::get('/edit',[ManagementUserController::class, 'edit']);
-Route::get('/update',[ManagementUserController::class, 'update']);
-Route::get('/destroy',[ManagementUserController::class, 'destroy']);
+Route::get('/user',[ManagementUserController::class, 'index']);
+//Route::get('/create',[ManagementUserController::class, 'create']);
+//Route::get('/store',[ManagementUserController::class, 'store']);
+//Route::get('/show',[ManagementUserController::class, 'show']);
+//Route::get('/edit',[ManagementUserController::class, 'edit']);
+//Route::get('/update',[ManagementUserController::class, 'update']);
+//Route::get('/destroy',[ManagementUserController::class, 'destroy']);
 
-Route::get('/home', function(){
-    return view("home");
-});
+//Route::get('/home', function(){return view("home");});
+
+//Route::get('/', function(){return view("welcome");});
+
+//Route::group(['namespace' => 'frontend'], function(){ Route::resource('home', 'HomeController');});
